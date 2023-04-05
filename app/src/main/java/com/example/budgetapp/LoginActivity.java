d462f3469155294eeef5f630b593ecdb1814e978
Kedoichi <haizzzchic@gmail.com>
2023-04-05 21:18:53 +1000

    Add Screen Flow + Slight Touch in UI

---
+++ app/src/main/java/com/example/budgetapp/LoginActivity.java
@@ -0,0 +1,32 @@
+package com.example.budgetapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in_page);
+        signUpScrChange();
+    }
+
+
+
+    private void signUpScrChange() {
+        TextView textView = findViewById(R.id.SignUpHereButton);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
