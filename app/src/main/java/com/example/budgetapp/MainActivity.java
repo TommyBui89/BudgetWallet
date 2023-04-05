373f4d111729138f64181ff2be303c29ba4fe0ef
tommybui <21023695@students.latrobe.edu.au>
2023-04-05 19:24:08 +1000

    create home page

--- app/src/main/java/com/example/budgetapp/MainActivity.java
+++ app/src/main/java/com/example/budgetapp/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sign_in_page);
+        setContentView(R.layout.sign_up_page);
     }
 }
