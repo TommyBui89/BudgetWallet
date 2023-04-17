2108dbb25b0bf4f653db6128f31d61d4c8912241
Vũ Tiến Quốc <42103573+Kedoichi@users.noreply.github.com>
2023-04-18 03:53:54 +1000

    FiseBase Login by using Google

--- app/src/main/java/com/example/budgetapp/LoginActivity.java
+++ app/src/main/java/com/example/budgetapp/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.example.budgetapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,15 +12,29 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity {
 
-    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    FirebaseAuth mAuth;
     TextInputEditText emailID, passwordID;
-    Button loginBTNID;
+    Button loginBTNID,googleBTN;
+    ProgressDialog progressDialog;
+
+    GoogleSignInClient mGoogleSignInClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,8 +47,22 @@ public class LoginActivity extends AppCompatActivity {
         emailID = findViewById(R.id.email);
         passwordID = findViewById(R.id.password);
         loginBTNID = findViewById(R.id.LoginButton);
+        googleBTN = findViewById(R.id.GoogleButton);
 
+        mAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(LoginActivity.this);
+        progressDialog.setTitle("Creating Account");
+        progressDialog.setMessage("We are creating account");
 
+        GoogleSignInOptions gso =new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(com.firebase.ui.auth.R.string.default_web_client_id)).requestEmail().build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this,gso);
+
+        googleBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signInUsingGoogle();
+            }
+        });
         loginBTNID.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
@@ -53,12 +84,45 @@ public class LoginActivity extends AppCompatActivity {
                         Toast.makeText(LoginActivity.this, "Authentication failed. Pls try again", Toast.LENGTH_SHORT).show();
                     }
                 });
-            }});
+            }
+        });
+        
+    }
+    int RC_SIGN_IN = 123;
+    private void signInUsingGoogle() {
+        Intent intent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(intent,RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==RC_SIGN_IN){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                fireBaseAuth(account.getIdToken());
+            }catch(ApiException e){}
+        }
+
 
 
     }
 
+    private void fireBaseAuth(String idToken) {
+        AuthCredential credential= GoogleAuthProvider.getCredential(idToken,null);
+        mAuth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    FirebaseUser user=mAuth.getCurrentUser();
 
+                    mainScrChange();
+                }
+            }
+        });
+    }
 
     public void signUpScrChange(View view) {
         Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
--- app/src/main/java/com/example/budgetapp/user.java
+++ app/src/main/java/com/example/budgetapp/user.java
@@ -1,5 +1,6 @@
 package com.example.budgetapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,50 +19,35 @@ import android.view.ViewGroup;
  */
 public class user extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    // Declare the sign out button
+    private Button btnSignOut;
+    private FirebaseAuth mAuth; // Firebase Authentication instance
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public user() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment user.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static user newInstance(String param1, String param2) {
-        user fragment = new user();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    // ...
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user, container, false);
+        View view = inflater.inflate(R.layout.fragment_user, container, false);
+
+        // Initialize FirebaseAuth
+        mAuth = FirebaseAuth.getInstance();
+
+        // Find the sign out button by its ID
+        btnSignOut = view.findViewById(R.id.btnSignOut);
+
+        // Set a click listener for the sign out button
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Call the signOut() method to sign out the user
+                mAuth.signOut();
+                // Optional: You can also navigate to your sign in activity or clear any user data here
+                startActivity(new Intent(getActivity(), LoginActivity.class));
+            }
+        });
+
+        return view;
     }
 }
