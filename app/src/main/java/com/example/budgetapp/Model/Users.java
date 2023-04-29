c49a083bb83970f2a5b85139557810a8340d44ce
Vũ Tiến Quốc <42103573+Kedoichi@users.noreply.github.com>
2023-04-29 23:01:59 +1000

    Finish picture pick and display user information

---
+++ app/src/main/java/com/example/budgetapp/Model/Users.java
@@ -0,0 +1,72 @@
+package com.example.budgetapp.Model;
+
+public class Users {
+    String userID, profilePic,firstName,lastName,phone,email,password;
+
+    public Users(String userID, String profilePic, String firstName, String lastName, String phone, String email, String password) {
+        this.userID = userID;
+        this.profilePic = profilePic;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getProfilePic() {
+        return profilePic;
+    }
+
+    public void setProfilePic(String profilePic) {
+        this.profilePic = profilePic;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public Users(){}
+}
