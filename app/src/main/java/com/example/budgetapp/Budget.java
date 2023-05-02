1e914f1e1e52381477caf1fc1d9076a47afcff7b
Vũ Tiến Quốc <42103573+Kedoichi@users.noreply.github.com>
2023-05-02 17:32:15 +1000

    update fragment structure

--- app/src/main/java/com/example/budgetapp/Budget.java
+++ app/src/main/java/com/example/budgetapp/Budget.java
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,44 +18,21 @@ import android.view.ViewGroup;
  */
 public class Budget extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    String url,firstName, lastName,email,phone,password;
+    CircleImageView avatar;
+    TextView name;
     public Budget() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Budget.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Budget newInstance(String param1, String param2) {
+        public static Budget newInstance() {
         Budget fragment = new Budget();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
